f922d642dff89d0f9d28293838ca95c381f71f45
Xwilarg <xwilarg@protonmail.com>
2025-02-26 19:05:15 +0100

    Add look direction gizmos

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -7,10 +7,17 @@ namespace ISN.Character
     public class PlayerController : ACharacter
     {
         public static PlayerController Instance { private set; get; }
+        private Vector2Int _lookDirection = Vector2Int.up;
 
         private void Awake()
         {
             Instance = this;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere((Vector2)transform.position + ((Vector2)_lookDirection).normalized * 1.28f, 1.28f / 2f);
         }
 
         private Vector2Int ToVector2Int(Vector2 v)
@@ -24,6 +31,7 @@ namespace ISN.Character
             {
                 var dir = ToVector2Int(value.ReadValue<Vector2>());
                 GridManager.Instance.TryMove(CurrentPosition, dir);
+                _lookDirection = dir;
             }
         }
     }
